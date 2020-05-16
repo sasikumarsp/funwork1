8df57c0ec9626586407a9220f71aa7a1dcff3130
sasikumarsp <sasisp2000@gmail.com>
2020-05-16 11:05:14 +0530

    make better

--- js/song.js
+++ js/song.js
@@ -83,6 +83,85 @@ $(document).ready(function () {
               });
        });
 
+       //Unnale en jeevan
+       //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/268572226&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+       SC.stream('/tracks/268572226', function (sound) {
+              $('#start-g1').click(function (e) {
+                     e.preventDefault();
+                     sound.start();
+              });
+              $('#stop-g1').click(function (e) {
+                     e.preventDefault();
+                     sound.stop();
+              });
+       });
+
+       //Hey Baby
+       //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/106315362&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+       SC.stream('/tracks/106315362', function (sound) {
+              $('#start-g2').click(function (e) {
+                     e.preventDefault();
+                     sound.start();
+              });
+              $('#stop-g2').click(function (e) {
+                     e.preventDefault();
+                     sound.stop();
+              });
+       });
+
+
+       //Love theme Polladavan
+       //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/96802530&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+       SC.stream('/tracks/96802530', function (sound) {
+              $('#start-g3').click(function (e) {
+                     e.preventDefault();
+                     sound.start();
+              });
+              $('#stop-g3').click(function (e) {
+                     e.preventDefault();
+                     sound.stop();
+              });
+       });
+
+
+       //Oru Pathi Kathavu
+       //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/58465747&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+       SC.stream('/tracks/58465747', function (sound) {
+              $('#start-g4').click(function (e) {
+                     e.preventDefault();
+                     sound.start();
+              });
+              $('#stop-g4').click(function (e) {
+                     e.preventDefault();
+                     sound.stop();
+              });
+       });
+
+       //Pirai Thedum Iravile
+       //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/38529221&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+       SC.stream('/tracks/38529221', function (sound) {
+              $('#start-g5').click(function (e) {
+                     e.preventDefault();
+                     sound.start();
+              });
+              $('#stop-g5').click(function (e) {
+                     e.preventDefault();
+                     sound.stop();
+              });
+       });
+
+       //Asuran Blood Path
+       //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/721444456&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+       SC.stream('/tracks/721444456', function (sound) {
+              $('#start-g6').click(function (e) {
+                     e.preventDefault();
+                     sound.start();
+              });
+              $('#stop-g6').click(function (e) {
+                     e.preventDefault();
+                     sound.stop();
+              });
+       });
 
 
 });
